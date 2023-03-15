5b834f432b502637b10a2b1b27780374753c15af
Vivek Sood <vsood@umd.edu>
2023-03-14 21:20:36 -0600

    seperate folders for different examples

---
+++ new_library/build/CMakeFiles/mainApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainApp.dir/main.cpp.o"
+  "mainApp"
+  "mainApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ new_library/build/CMakeFiles/testRunner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testRunner.dir/test.cpp.o"
+  "testRunner"
+  "testRunner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testRunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
